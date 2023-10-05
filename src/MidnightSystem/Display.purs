ae3be9d68b1eb473856eab438f293b9ba0f8632f
Ian Grant Jeffries <mail@ianjeffries.net>
2023-10-05 12:36:49 -0400

    Fix max display size

--- src/MidnightSystem/Display.purs
+++ src/MidnightSystem/Display.purs
@@ -2,16 +2,26 @@ module MidnightSystem.Display where
 
 import Prelude
 
+import Data.Array (fromFoldable)
+import Data.Array as Array
 import Data.Either (Either(..))
 import Data.Foldable (fold)
-import Data.Array (fromFoldable)
 import Data.Int as Int
 import Data.List (List)
 import Data.List ((:))
 import Data.List as PsList
+import Data.String as String
 import Data.Traversable (for)
 import MidnightLang.Sexp (Sexp)
 import MidnightLang.Sexp as Sexp
+
+displayWidth :: Int
+displayWidth =
+  80
+
+displayHeight :: Int
+displayHeight =
+  30
 
 type Display =
   { cursorPosition :: { x :: Int, y :: Int }
@@ -23,7 +33,7 @@ parse =
   case _ of
     Sexp.List (cursorPositionSexp : textSexp : PsList.Nil) -> do
       cursorPosition <- parseCursorPosition cursorPositionSexp
-      text <- parseText textSexp
+      text <- clipToFit <$> parseText textSexp
       pure { cursorPosition: cursorPosition, text: text }
 
     other ->
@@ -33,12 +43,12 @@ parseCursorPosition :: Sexp -> Either String { x :: Int, y :: Int }
 parseCursorPosition =
   case _ of
     Sexp.List (Sexp.Symbol "cursor-position" : Sexp.Int x : Sexp.Int y : PsList.Nil) -> do
-      Right { x, y}
+      Right { x, y }
 
     other ->
       Left ("Display cursor-position incorrect: " <> Sexp.print other)
 
-parseText  :: Sexp -> Either String (Array String)
+parseText :: Sexp -> Either String (Array String)
 parseText =
   case _ of
     Sexp.List (Sexp.Symbol "text" : Sexp.List lines : PsList.Nil) ->
@@ -46,3 +56,7 @@ parseText =
 
     other ->
       Left ("Display text field incorrect: " <> Sexp.print other)
+
+clipToFit :: Array String -> Array String
+clipToFit xs =
+  String.take displayWidth <$> Array.take displayHeight xs
