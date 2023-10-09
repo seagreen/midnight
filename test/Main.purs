d6a9a707441d76244b1167a9833f1982e1625d1f
Ian Grant Jeffries <mail@ianjeffries.net>
2023-10-09 18:10:45 -0400

    Remove biwa tests

--- test/Main.purs
+++ test/Main.purs
@@ -5,8 +5,6 @@ import Prelude
 import Effect (Effect)
 import Effect.Aff (launchAff_)
 import Test.Lib.Sexp as Test.Lib.Sexp
-import Test.MidnightBiwa as Test.MidnightBiwa
-import Test.MidnightBiwa.Foreign as Test.MidnightBiwa.Foreign
 import Test.MidnightJS as Test.MidnightJS
 import Test.MidnightSystem as Test.MidnightSystem
 import Test.Spec (describe)
@@ -18,6 +16,4 @@ main = do
   launchAff_ $ runSpec [ consoleReporter ] do
     describe "sexpr" Test.Lib.Sexp.spec
     describe "midnight-js" Test.MidnightJS.spec
-    describe "midnight-biwa-foreign" Test.MidnightBiwa.Foreign.spec
-    describe "midnight-biwa" Test.MidnightBiwa.spec
     describe "midnight-vm" Test.MidnightSystem.spec
